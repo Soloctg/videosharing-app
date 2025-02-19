e10473047a30d757f1063708a1f937e37f5c65f8
solomon-ctg <solomon.iroegbu@gmail.com>
2025-02-19 19:32:58 +0100

    feat: Add React Material-UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 
 import './App.css';
 
+import React from "react";
+
+import Grid from "@mui/material/Grid";
+//import youtube from "./api/youtube";
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       Hello
-      </header>
-    </div>
+      <Grid style={{ justifyContent: "center" }} container spacing={10}>
+        <Grid item xs={11}>
+            <Grid container spacing={10}>
+                <Grid item xs={12}>
+                    {/* {Search} */}
+                </Grid>
+
+                <Grid xs={8}>
+                    {/* {VideoDetail} */}
+                </Grid>
+
+                <Grid xs={4}>
+                    {/* {VideoList} */}
+                </Grid>
+
+
+            </Grid>
+        </Grid>
+      </Grid>
   );
 }
 
