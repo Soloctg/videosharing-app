a7f2d3636c55a3edce8a883f077e31b8b305688d
solomon-ctg <solomon.iroegbu@gmail.com>
2025-02-20 18:16:51 +0100

    feat: SearchBar Component

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import React from "react";
 
 import Grid from "@mui/material/Grid";
 import youtube from "./api/youtube";
+import SearchBar from './components/SearchBar';
+import { useState } from "react";
+
 
 
 function App() {
+	const [videos, setVideos] = useState([]);
+	const [selectedVideo, setSelectedVideo] = useState({ id: {}, snippet: {} });
+
   return (
       <Grid style={{ justifyContent: "center" }} container spacing={10}>
         <Grid item xs={11}>
             <Grid container spacing={10}>
                 <Grid item xs={12}>
-                    {/* {Search} */}
-                    Video Sharing App
+	                <SearchBar onSubmit={handleSubmit} />
                 </Grid>
 
                 <Grid xs={8}>
@@ -30,6 +35,19 @@ function App() {
         </Grid>
       </Grid>
   );
+
+	async function handleSubmit(searchTerm) {
+		const { data: { items: videos } } = await youtube.get("search", {
+			params: {
+				part: "snippet",
+				maxResults: 5,
+				key: "AIzaSyDVtGGEZDHUj3Mmma8IQYYuYK-pMT1YN30",
+				q: searchTerm,
+			}
+		});
+		setVideos(videos);
+		setSelectedVideo(videos[0]);
+	}
 }
 
 export default App;
